eda19692ece65b5e992fedb827d98499fb833e1b
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-03 09:57:46 +0200

    Making a count app

--- day-003/tkinter-tutorial/main.py
+++ day-003/tkinter-tutorial/main.py
@@ -1,22 +1,28 @@
 import tkinter as tk
 
+
+def increase():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value + 1}"
+
+
+def decrease():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value - 1}"
+
+
 window = tk.Tk()
-window.title("My Countdown App")
 
-for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=75)
-    window.rowconfigure(i, weight=1, minsize=50)
+window.rowconfigure(0, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
-    for j in range(3):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
+btn_decrease = tk.Button(master=window, text="-", command=decrease)
+btn_decrease.grid(row=0, column=0, sticky="nsew")
 
-        frame.grid(row=i, column=j, padx=5, pady=5, )
-        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label.pack(padx=5, pady=5)
+lbl_value = tk.Label(master=window, text="0")
+lbl_value.grid(row=0, column=1)
 
+btn_increase = tk.Button(master=window, text="+", command=increase)
+btn_increase.grid(row=0, column=2, sticky="nsew")
 
 window.mainloop()
