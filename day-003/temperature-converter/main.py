39f90832250aff738fb18de9610d61fb5246d7a3
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-03 10:22:19 +0200

    Temperature Converter App

---
+++ day-003/temperature-converter/main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+def fahrenheit_to_celsius():
+    fahrenheit = ent_temperature.get()
+    celsius = (5 / 9) * (float(fahrenheit) - 32)
+    lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
+
+
+window = tk.Tk()
+window.title("Temperature Converter")
+
+frm_entry = tk.Frame(master=window)
+ent_temperature = tk.Entry(master=frm_entry, width=10)
+lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
+
+ent_temperature.grid(row=0, column=0, sticky="e")
+lbl_temp.grid(row=0, column=1, sticky="w")
+
+btn_convert = tk.Button(
+    master=window,
+    text="\N{RIGHTWARDS BLACK ARROW}",
+    command=fahrenheit_to_celsius
+)
+lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
+
+frm_entry.grid(row=0, column=0, padx=50, pady=50)
+btn_convert.grid(row=0, column=1, pady=50)
+lbl_result.grid(row=0, column=2, padx=50, pady=50)
+
+window.mainloop()
