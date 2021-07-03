7309ba14704ce4e08d663a758a4aac98c34a4516
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-03 09:50:24 +0200

    Playing with grid

--- day-003/tkinter-tutorial/main.py
+++ day-003/tkinter-tutorial/main.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.title("My Countdown App")
 
-label = tk.Label(
-    text="Name"
-)
+for i in range(3):
+    window.columnconfigure(i, weight=1, minsize=75)
+    window.rowconfigure(i, weight=1, minsize=50)
 
-entry = tk.Entry()
+    for j in range(3):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
 
-name = entry.get()
+        frame.grid(row=i, column=j, padx=5, pady=5, )
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack(padx=5, pady=5)
 
-entry.insert(0, "Python")
-
-label.pack()
-entry.pack()
 
 window.mainloop()
