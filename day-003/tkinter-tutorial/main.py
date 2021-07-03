c2e374eab97fffea37c9e00616687f6f2b9726d3
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-03 09:11:30 +0200

    Making a button widget with Tkinter

--- day-003/tkinter-tutorial/main.py
+++ day-003/tkinter-tutorial/main.py
@@ -2,14 +2,14 @@ import tkinter as tk
 
 window = tk.Tk()
 
-greeting = tk.Label(
-    text="Hello Tkinter",
-    fg="#34A2FE",     # set the text color to light blue
-    bg="black",      # set the background color to black
-    width=10,
-    height=5        # tkinter uses the 0 to set the hight and width instead of pixels
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    fg="yellow",
+    bg="blue"
 )
 
-greeting.pack()
+button.pack()
 
 window.mainloop()
